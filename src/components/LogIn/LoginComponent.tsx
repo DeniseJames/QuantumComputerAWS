2488c8697edc12190e14bf8490d70cc604743785
Denise James <denisetoo@gmail.com>
2024-09-07 06:25:31 -0500

    Add Login routing to home page

--- src/components/LogIn/LoginComponent.tsx
+++ src/components/LogIn/LoginComponent.tsx
@@ -1,21 +1,33 @@
-
 import styles from './LoginComponent.module.css';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const LoginComponent: React.FC = () => {
     const { signOut, user } = useAuthenticator((context) => [context.user]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            navigate('/');
+        }
+    }, [user, navigate]);
+
+    const handleSignOut = () => {
+        signOut();
+        navigate('/');
+    };
 
     return (
-        
         <div className={styles.authButton}>
-        {user ? (
-          <button onClick={signOut} className="btn btn-warning mx-2">Log off</button>
-        ) : (
-          <Authenticator>
-            <button className="btn btn-warning mx-2">Login</button>
-          </Authenticator>
-        )}
-      </div>
+            {user ? (
+                <button onClick={handleSignOut} className="btn btn-warning mx-2">Log off</button>
+            ) : (
+                <Authenticator>
+                    <button className="btn btn-warning mx-2">Login</button>
+                </Authenticator>
+            )}
+        </div>
     );
 };
 
